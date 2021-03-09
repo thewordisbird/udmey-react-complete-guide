58c435a949baf56a3ea7c229b2cba7bc323b97dc
Justin Bird <justin@thewordisbird.dev>
2021-03-09 06:52:19 -0800

    Section 16: Redux Advanced

--- section_16/redux-adv--01-start/src/store/reducers/result.js
+++ section_16/redux-adv--01-start/src/store/reducers/result.js
@@ -5,13 +5,18 @@ const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, {results: updatedArray})
+}
+
+
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.STORE_RESULT:
             return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})})
         case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return updateObject(state, {results: updatedArray})
+            return deleteResult(state, action)            
     }
     return state;
 };
